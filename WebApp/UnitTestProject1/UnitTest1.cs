7253a3d733fdf569624bab83b56d1e27b552fece
MinaevaMV <minaevam87@gmail.com>
2018-11-26 17:51:11 +0300

    db+models

---
+++ WebApp/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApp;
+using WebApp.Controllers;
+using WebApp.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Index()
+        {
+           
+        }
+   
+    }
+}
---
+++ WebApp/WebApp/Controllers/ChildController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ChildController : Controller
+    {
+        private readonly child _dao = new child();
+        // GET: Child
+        public ActionResult Index()
+        {
+            return View(child.GetChild());
+        }
+
+
+        // GET: Child/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public ActionResult Create(child child1)
+        {
+            try
+            {
+
+                _dao.CreateChildData(child1);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+        // GET: Child/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Child/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Child/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Child/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
